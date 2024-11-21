d3d3ed89a9c269e9e3a7ae4a3480e610d6adf677
ROBERTTest1 <robi.tarlap123@gmail.com>
2024-11-21 20:17:59 +0200

    Kalkar tootab nuuud

---
+++ BitcoinCalculator/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace BitcoinCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.currencyselector = new System.Windows.Forms.ComboBox();
+            this.arvutus = new System.Windows.Forms.Button();
+            this.bitcoinamountinput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.suva = new System.Windows.Forms.Label();
+            this.tulemuslabel = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // currencyselector
+            // 
+            this.currencyselector.FormattingEnabled = true;
+            this.currencyselector.Items.AddRange(new object[] {
+            "EUR",
+            "USD",
+            "EEK"});
+            this.currencyselector.Location = new System.Drawing.Point(607, 149);
+            this.currencyselector.Name = "currencyselector";
+            this.currencyselector.Size = new System.Drawing.Size(121, 21);
+            this.currencyselector.TabIndex = 0;
+            this.currencyselector.Text = "Vali Valuuta";
+            // 
+            // arvutus
+            // 
+            this.arvutus.Location = new System.Drawing.Point(607, 186);
+            this.arvutus.Name = "arvutus";
+            this.arvutus.Size = new System.Drawing.Size(121, 55);
+            this.arvutus.TabIndex = 1;
+            this.arvutus.Text = "Arvuta kurss";
+            this.arvutus.UseVisualStyleBackColor = true;
+            this.arvutus.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // bitcoinamountinput
+            // 
+            this.bitcoinamountinput.Location = new System.Drawing.Point(607, 112);
+            this.bitcoinamountinput.Name = "bitcoinamountinput";
+            this.bitcoinamountinput.Size = new System.Drawing.Size(121, 20);
+            this.bitcoinamountinput.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(607, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Sisesta Bitcoini arv";
+            // 
+            // suva
+            // 
+            this.suva.AutoSize = true;
+            this.suva.BackColor = System.Drawing.Color.Transparent;
+            this.suva.ForeColor = System.Drawing.Color.Lime;
+            this.suva.Location = new System.Drawing.Point(607, 282);
+            this.suva.Name = "suva";
+            this.suva.Size = new System.Drawing.Size(53, 13);
+            this.suva.TabIndex = 4;
+            this.suva.Text = "Tulemus: ";
+            // 
+            // tulemuslabel
+            // 
+            this.tulemuslabel.BackColor = System.Drawing.Color.Black;
+            this.tulemuslabel.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tulemuslabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.tulemuslabel.ForeColor = System.Drawing.Color.Lime;
+            this.tulemuslabel.Location = new System.Drawing.Point(607, 298);
+            this.tulemuslabel.Name = "tulemuslabel";
+            this.tulemuslabel.Size = new System.Drawing.Size(121, 49);
+            this.tulemuslabel.TabIndex = 5;
+            this.tulemuslabel.Text = "Pole";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tulemuslabel);
+            this.Controls.Add(this.suva);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bitcoinamountinput);
+            this.Controls.Add(this.arvutus);
+            this.Controls.Add(this.currencyselector);
+            this.Name = "Form1";
+            this.Text = "BitcoinCalculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox currencyselector;
+        private System.Windows.Forms.Button arvutus;
+        private System.Windows.Forms.TextBox bitcoinamountinput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label suva;
+        private System.Windows.Forms.TextBox tulemuslabel;
+    }
+}
+
---
+++ BitcoinCalculator/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace BitcoinCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (currencyselector.SelectedItem.ToString() == "EUR")
+            {
+                arvutus.Visible = true;
+                suva.Visible = true;
+                BitcoinRates newbitcoinrate = GetRates();
+                float result = float.Parse(bitcoinamountinput.Text) * (float)newbitcoinrate.Bpi.EUR.rate_float;
+                tulemuslabel.Text = $"{result} Bitcoini {newbitcoinrate.Bpi.EUR.code}";
+            }
+        }
+
+        public static BitcoinRates GetRates()
+        {
+            string url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            BitcoinRates bitcoin;
+            using (var responseReader = new StreamReader(webStream))
+            {
+                var data = responseReader.ReadToEnd();
+                bitcoin = JsonConvert.DeserializeObject<BitcoinRates>(data);
+            }  
+            return bitcoin;
+        }
+    }
+}
